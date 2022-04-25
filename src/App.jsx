bf4150b7bc841a19d29e2dc64e49e8ea5dfcabe8
Bekir - CDI <b.burakcebecii@gmail.com>
2022-04-25 10:13:12 +0200

    for every job status to accepted fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import {useState} from `react`;
 import './App.scss';
-import _jobs from `./data/data.json`
+import _jobs from `./data/data.json`;
+
+_jobs.forEach((job)=> {
+  job.status=`accepted`
+});
 
 function App() {
 	return <div className="App">Hello!</div>;
